b23f70b904a0732c23208b68ec0498f9561ee3f0
Niko Heiskanen <henipe@gmail.com>
2025-02-12 21:40:18 +0200

    add keyboard9000

---
+++ keyboards/handwired/keyboard9000/config.h
@@ -0,0 +1,7 @@
+#pragma once
+
+#define EE_HANDS // Store which side I am in EEPROM
+
+/* Keyboard matrix assignments */
+#define MATRIX_ROW_PINS { GP21, GP20, GP19, GP18, GP10 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7, GP8 }
---
+++ keyboards/handwired/keyboard9000/rules.mk
@@ -0,0 +1,11 @@
+# Build Options
+#   change yes to no to disable
+
+AUDIO_SUPPORTED = no
+RGB_MATRIX_SUPPORTED = no
+RGBLIGHT_SUPPORTED = no
+BOOTMAGIC_ENABLE = yes
+EXTRAKEY_ENABLE = yes
+SPLIT_KEYBOARD = yes
+SERIAL_DRIVER = vendor
+VIA_ENABLE = yes
