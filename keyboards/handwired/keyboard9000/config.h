e4b6772bcb3bba16d31f3ccf9686b08f6f494661
Niko Heiskanen <henipe@gmail.com>
2025-02-16 22:31:02 +0200

    (keyboard9000): refactor for rp2040

--- keyboards/handwired/keyboard9000/config.h
+++ keyboards/handwired/keyboard9000/config.h
@@ -4,6 +4,8 @@
 
 /* Keyboard matrix assignments */
 // https://imgur.com/wMNx2u6
-#define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
-#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
-#define DYNAMIC_KEYMAP_LAYER_COUNT 5
+// #define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
+// #define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
+#define MATRIX_ROW_PINS { GP29, GP28, GP27, GP26, GP15, GP14 }
+#define MATRIX_COL_PINS { GP2, GP3, GP4, GP5, GP6, GP7, GP8 }
+#define DYNAMIC_KEYMAP_LAYER_COUNT 12
--- keyboards/handwired/keyboard9000/keymaps/via/keymap.c
+++ keyboards/handwired/keyboard9000/keymaps/via/keymap.c
@@ -8,9 +8,9 @@ enum custom_layers {
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_QWERTY] = LAYOUT(
         _______,_______,_______,_______,_______,_______,_______,
-        _______,_______,KC_Q, KC_W  , KC_E  , KC_R  , KC_T  ,                            KC_Y,      KC_U  ,      KC_I  , KC_O       , KC_P          ,KC_BSPC,
-        _______,KC_TAB ,LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LGUI_T(KC_F),KC_G,        KC_H,RGUI_T(KC_J),LALT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN),KC_QUOT,
-        _______,KC_CAPS, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,                         KC_N,      KC_M  ,     KC_COMM,KC_DOT      ,KC_SLSH        ,KC_SLSH,
+        _______,KC_Q, KC_W  , KC_E  , KC_R  , KC_T  ,_______,                            KC_Y,      KC_U  ,      KC_I  , KC_O       , KC_P          ,KC_BSPC,
+        KC_TAB ,LSFT_T(KC_A),LCTL_T(KC_S),LALT_T(KC_D),LGUI_T(KC_F),KC_G,_______,        KC_H,RGUI_T(KC_J),LALT_T(KC_K),RCTL_T(KC_L),RSFT_T(KC_SCLN),KC_QUOT,
+        KC_CAPS, KC_Z  , KC_X  , KC_C  , KC_V  , KC_B  ,_______,                         KC_N,      KC_M  ,     KC_COMM,KC_DOT      ,KC_SLSH        ,KC_SLSH,
                          KC_LBRC,KC_RBRC,
                                            _______,KC_SPC,KC_HOME,                           KC_ENT,_______,KC_TAB,
                                            KC_BSPC,LT(_NAV, KC_ESC),_______
