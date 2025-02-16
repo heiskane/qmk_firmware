e46eafb364c57a7319ac75494beb00120402e30a
Niko Heiskanen <henipe@gmail.com>
2025-02-16 14:52:46 +0200

    (keyboard9000): functioning via setup

--- keyboards/handwired/keyboard9000/config.h
+++ keyboards/handwired/keyboard9000/config.h
@@ -5,4 +5,4 @@
 /* Keyboard matrix assignments */
 // https://imgur.com/wMNx2u6
 #define MATRIX_ROW_PINS { F4, F5, F6, F7, B1, B3 }
-#define MATRIX_COL_PINS { D1, D0, C6, C6, D7, E6, B4 }
+#define MATRIX_COL_PINS { D1, D0, D4, C6, D7, E6, B4 }
