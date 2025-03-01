d6af1c013251ac845217e6b1e809a992b75f5bbd
Niko Heiskanen <henipe@gmail.com>
2025-03-01 19:54:03 +0200

    [keyboard9000]: cleanup

--- keyboards/handwired/keyboard9000/rules.mk
+++ keyboards/handwired/keyboard9000/rules.mk
@@ -1,12 +1,8 @@
-# Build Options
-#   change yes to no to disable
-
 AUDIO_SUPPORTED = no
 RGB_MATRIX_SUPPORTED = no
 RGBLIGHT_SUPPORTED = no
 BOOTMAGIC_ENABLE = yes
 EXTRAKEY_ENABLE = yes
 SPLIT_KEYBOARD = yes
-# SERIAL_DRIVER = bitbang
 SERIAL_DRIVER = vendor
 VIA_ENABLE = yes
